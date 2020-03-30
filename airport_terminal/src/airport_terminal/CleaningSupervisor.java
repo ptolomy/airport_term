97fb37203e1a5d2e3c72297516fe27eb74c855a2
Will-Oneill <williamoneill96@outlook.com>
2020-03-30 16:23:04 +0100

    CleaningSupervisor Update

--- airport_terminal/src/airport_terminal/CleaningSupervisor.java
+++ airport_terminal/src/airport_terminal/CleaningSupervisor.java
@@ -1,6 +1,7 @@
 package airport_terminal;
 
 import java.awt.Container;
+import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -8,35 +9,55 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 
 /**
- * An interface to SAAMS:
- * Cleaning Supervisor Screen:
- * Inputs events from the Cleaning Supervisor, and displays aircraft information.
- * This class is a controller for the AircraftManagementDatabase: sending it messages to change the aircraft status information.
- * This class also registers as an observer of the AircraftManagementDatabase, and is notified whenever any change occurs in that <<model>> element.
- * See written documentation.
+ * An interface to SAAMS: Cleaning Supervisor Screen: Inputs events from the
+ * Cleaning Supervisor, and displays aircraft information. This class is a
+ * controller for the AircraftManagementDatabase: sending it messages to change
+ * the aircraft status information. This class also registers as an observer of
+ * the AircraftManagementDatabase, and is notified whenever any change occurs in
+ * that <<model>> element. See written documentation.
  * 
  */
-public class CleaningSupervisor extends JFrame{
-/**
-  * The Cleaning Supervisor Screen interface has access to the AircraftManagementDatabase.
-  * @clientCardinality 1
-  * @supplierCardinality 1
-  * @label accesses/observes
-  * @directed*/
-  private AircraftManagementDatabase lnkUnnamed;
-  private String title = "Cleaning Supervisor";
-  //private JButton clean;
-  
+public class CleaningSupervisor extends JFrame implements ActionListener {
+	/**
+	 * The Cleaning Supervisor Screen interface has access to the
+	 * AircraftManagementDatabase.
+	 * 
+	 * @clientCardinality 1
+	 * @supplierCardinality 1
+	 * @label accesses/observes
+	 * @directed
+	 */
+	private AircraftManagementDatabase lnkUnnamed;
+	private String title = "Cleaning Supervisor";
 
-  public CleaningSupervisor() {
-	  
-	  setTitle(title);
-	  setLocationRelativeTo(null);
-	  setSize(400,200); // change to suit preffered size
-	  setDefaultCloseOperation(DO_NOTHING_ON_CLOSE);
+	private JButton quit;
 
-	  setVisible(true);
-  }
+	// private JButton clean;
 
+	public CleaningSupervisor() {
+
+		setTitle(title);
+		setLocationRelativeTo(null);
+		setSize(400, 200); // change to suit preffered size
+		setDefaultCloseOperation(DO_NOTHING_ON_CLOSE);
+		
+		
+		Container window = getContentPane();
+        window.setLayout(new FlowLayout());
+
+		quit = new JButton("Quit");
+		window.add(quit);
+		quit.addActionListener(this);
+		
+		setVisible(true);
+	}
+
+	// Button click handling:
+	public void actionPerformed(ActionEvent e) {
+
+		if (e.getSource() == quit)
+			System.exit(0);
+
+	} // actionPerformed
 
 }
