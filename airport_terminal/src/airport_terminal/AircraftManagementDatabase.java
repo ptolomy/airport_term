84c18423dbe701d178463beec27420f33943b815
Will-Oneill <williamoneill96@outlook.com>
2020-03-30 21:03:16 +0100

    AircraftManagementDatabase Update

--- airport_terminal/src/airport_terminal/AircraftManagementDatabase.java
+++ airport_terminal/src/airport_terminal/AircraftManagementDatabase.java
@@ -60,14 +60,11 @@ public class AircraftManagementDatabase {
 	 * public operations for requesting specific status changes.
 	 */
 	public void setStatus(int mCode, int newStatus) {
+		
 		try {
-			for (int i = 0; i < MRs.length; i++) {
-				if (MRs[i].getStatus() == newStatus) {
-					break;
-				} else {
-					MRs[i].setStatus(newStatus); // hrmm feels wrong
-				}
-			}
+			MRs[mCode].setStatus(newStatus);
+			
+			
 		} catch (IndexOutOfBoundsException ex) {
 			ex.printStackTrace();
 		}
@@ -186,10 +183,7 @@ public class AircraftManagementDatabase {
 	 * Return the PassengerList of the aircraft with the given mCode.
 	 */
 	public PassengerList getPassengerList(int mCode) {
-		for (int i = 0; i < MRs.length; i++) {
-			return MRs[i].getPassengerList();
-		}
-		return null;
+			return MRs[mCode].getPassengerList();
 	}
 
 	/**
