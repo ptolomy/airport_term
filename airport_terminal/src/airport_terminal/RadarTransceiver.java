0ac5df531eb59e4d61a95162dcf4abc9f014f4c5
Michael Dempster <Michael Dempster@192.168.1.5>
2020-03-31 23:08:26 +0100

    Radar Transceiver In Progress

--- airport_terminal/src/airport_terminal/RadarTransceiver.java
+++ airport_terminal/src/airport_terminal/RadarTransceiver.java
@@ -3,6 +3,7 @@ package airport_terminal;
 import java.awt.Container;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
 import java.util.Observable;
 import java.util.Observer;
 
@@ -31,18 +32,27 @@ public class RadarTransceiver extends JFrame implements ActionListener, Observer
 	 */
 	private AircraftManagementDatabase aircraftManagementDatabase;	
 
-	private JTextField flightCodeText; //Create a textfield for the flight code to be typed in to
-	private JTextField fromText;
+	private JTextField flightCodeText; //Create a text field for the flight code to be typed in to
+	private JTextField fromText;//Create a text field for the from option
+	private JTextField toText;//Create a text field for the to option
+	private JTextField nextText;//Create a text field for the next option on the itinerary
+	private JTextField namesText;//Create a text field for the names of the passengers to be typed in to
 	
-	private JTextField toText;
+	private JButton addPassengerButton;
+	private JList passengerList;
+	private JScrollPane scrollList;
 	
-	
-	
-	
+	private JButton detectFlightButton;
 
+	private PassengerList passengers;
+	private ArrayList<Integer> mCodes;
+	
 	public RadarTransceiver(AircraftManagementDatabase amd) {
 		this.aircraftManagementDatabase = amd;
 		amd.addObserver(this);
+		
+		mCodes = new ArrayList<Integer>();
+		passengers = new PassengerList();
 		
 		//Code to initialise the GUI
 		setTitle("Radar Transceiver");
@@ -50,7 +60,39 @@ public class RadarTransceiver extends JFrame implements ActionListener, Observer
 		setSize(400, 200); // change to suit preferred size
 		setDefaultCloseOperation(DO_NOTHING_ON_CLOSE);
 
+		JPanel transceiver = new JPanel(); //Create a new JPanel for the transceiver information to appear on
 		
+		JLabel flightCodelbl = new JLabel("Flight Code:");
+		transceiver.add(flightCodelbl);
+		
+		flightCodeText = new JTextField(15);
+		transceiver.add(flightCodeText);
+		
+		JLabel itineraryInfolbl = new JLabel("Itinerary:");
+		transceiver.add(itineraryInfolbl);
+		
+		JLabel fromlbl = new JLabel("From:");
+		transceiver.add(fromlbl);
+		
+		fromText = new JTextField(20);
+		transceiver.add(fromText);
+		
+		JLabel tolbl = new JLabel("To:");
+		transceiver.add(tolbl);
+		
+		toText = new JTextField(20);
+		transceiver.add(toText);
+		
+		JLabel nextlbl = new JLabel("Next:");
+		transceiver.add(tolbl);
+		
+		nextText = new JTextField(20);
+		transceiver.add(nextText);
+		
+		
+		
+		JLabel passengerListlbl = new JLabel("Passenger List:");
+		transceiver.add(passengerListlbl);
 		
 		
 		
