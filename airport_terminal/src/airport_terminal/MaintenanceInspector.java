25afccb31c5b4c9bc8625a3965310be1501c6c08
Will-Oneill <williamoneill96@outlook.com>
2020-03-31 23:06:22 +0100

    MaintenanceInspector Update

--- airport_terminal/src/airport_terminal/MaintenanceInspector.java
+++ airport_terminal/src/airport_terminal/MaintenanceInspector.java
@@ -7,36 +7,36 @@ import java.awt.event.ActionListener;
 import javax.swing.JFrame;
 
 /**
- * An interface to SAAMS:
- * Maintenance Inspector Screen:
- * Inputs events from the Maintenance Inspector, and displays aircraft information.
- * This class is a controller for the AircraftManagementDatabase: sending it messages to change the aircraft status information.
- * This class also registers as an observer of the AircraftManagementDatabase, and is notified whenever any change occurs in that <<model>> element.
- * See written documentation.
+ * An interface to SAAMS: Maintenance Inspector Screen: Inputs events from the
+ * Maintenance Inspector, and displays aircraft information. This class is a
+ * controller for the AircraftManagementDatabase: sending it messages to change
+ * the aircraft status information. This class also registers as an observer of
+ * the AircraftManagementDatabase, and is notified whenever any change occurs in
+ * that <<model>> element. See written documentation.
  */
 @SuppressWarnings("serial")
 public class MaintenanceInspector extends JFrame {
-/**  The Maintenance Inspector Screen interface has access to the AircraftManagementDatabase.
-  * @clientCardinality 1
-  * @supplierCardinality 1
-  * @label accesses/observes
-  * @directed*/
-  private AircraftManagementDatabase aircraftManagementDatabase;
-  private String title = "Maintenance Inspector";
-  
+	/**
+	 * The Maintenance Inspector Screen interface has access to the
+	 * AircraftManagementDatabase.
+	 * 
+	 * @clientCardinality 1
+	 * @supplierCardinality 1
+	 * @label accesses/observes
+	 * @directed
+	 */
 
-  public MaintenanceInspector(AircraftManagementDatabase amd) {
-	  this.aircraftManagementDatabase = amd;
+	private AircraftManagementDatabase aircraftManagementDatabase;
 
-	  setTitle(title);
-	  setLocationRelativeTo(null);
-	  setSize(400,200); // change to suit preffered size
-	  setDefaultCloseOperation(DO_NOTHING_ON_CLOSE);
+	public MaintenanceInspector(AircraftManagementDatabase amd) {
+		this.aircraftManagementDatabase = amd;
 
-	  setVisible(true);
-  }
+		setTitle("Maintenance Inspector");
+		setLocationRelativeTo(null);
+		setSize(400, 200); // change to suit preffered size
+		setDefaultCloseOperation(DO_NOTHING_ON_CLOSE);
 
-  
-  
+		setVisible(true);
+	}
 
 }
